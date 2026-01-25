2423fcad11a42acaca3bda33eeb47fd7dfdcfbda
Anirudh1905 <anirudhsridhar1905@gmail.com>
2026-01-25 17:40:53 +0530

    refactor: remove Redis caching from data ingestion endpoint

--- src/routers/data.py
+++ src/routers/data.py
@@ -1,23 +1,19 @@
-from datetime import timedelta
 import json
 import logging
 from fastapi import APIRouter, HTTPException
 from fastapi.encoders import jsonable_encoder
-import redis
 import boto3
-from constants import DATABASE_URL, REDIS_HOST, REDIS_PORT, STREAM_NAME
+from constants import DATABASE_URL, STREAM_NAME
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from data_schema import ChurnData, ResponseModel, TelecomUsers, Base
 
-redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
 kinesis_client = boto3.client("kinesis", region_name="us-east-1")
 data_router = APIRouter(prefix="/data")
 
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
 Base.metadata.create_all(engine, checkfirst=True)
-CACHE_EXPIRATION_TIME = timedelta(days=1)
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -28,9 +24,7 @@ async def send_data(user: ChurnData) -> ResponseModel:
     """
     Ingest user data and send it to an AWS Kinesis stream.
 
-    This endpoint ingests user data, sends it to an AWS Kinesis stream, and caches the data in Redis.
-    If the data is already cached, it returns a response indicating that the data is already in the stream.
-    If the data is successfully sent to the stream, it updates the cache.
+    This endpoint ingests user data and sends it to an AWS Kinesis stream.
 
     Args:
         user (ChurnData): The user churn data to be ingested.
@@ -38,14 +32,6 @@ async def send_data(user: ChurnData) -> ResponseModel:
     Returns:
         ResponseModel: A response model containing the status, cache status, and response data.
     """
-    redis_key_id = f"user:{user.customerID}:{user.gender}:{user.SeniorCitizen}:{user.Partner}:{user.Dependents}:{user.tenure}:{user.PhoneService}:{user.MultipleLines}:{user.InternetService}:{user.OnlineSecurity}"
-    cached_data = redis_client.get(redis_key_id)
-
-    if cached_data:
-        return ResponseModel(
-            status="Success", cached=True, response={"result": "Data already in stream"}
-        )
-
     response = kinesis_client.put_record(
         StreamName=STREAM_NAME,
         Data=json.dumps(user.dict()),
@@ -58,13 +44,6 @@ async def send_data(user: ChurnData) -> ResponseModel:
             cached=False,
             response={"result": "Failed to add data to stream"},
         )
-
-    redis_client.delete(redis_key_id)
-    redis_client.set(
-        redis_key_id,
-        json.dumps(user.dict()),
-        ex=int(CACHE_EXPIRATION_TIME.total_seconds()),
-    )
 
     return ResponseModel(
         status="Success", cached=False, response={"result": "Data added to stream"}
